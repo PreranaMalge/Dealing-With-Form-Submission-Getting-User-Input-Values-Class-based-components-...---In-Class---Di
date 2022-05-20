17d21e08b5df3bb5ff43efa58aef50d1d4c217df
PreranaMalge <preranamalge123@gmail.com>
2022-05-20 17:19:34 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  const [timedate,setTimeDate] = useState("");
+
+  const handleTime =  () =>{
+    let puretime = new Date().toLocaleString();
+    setTimeDate(puretime);
+  }
+
+  useEffect (()=>{
+    let puretime = new Date().toLocaleString();
+    setTimeDate(puretime);
+    let result = setInterval(handleTime,1000);
+    return () =>{
+      clearInterval(result);
+    }
+  },[])
+
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">
+        {timedate}
+      </div>
     </div>
   )
 }
